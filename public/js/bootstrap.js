a56a16ff4555c2b2787d59bf81484f5510054aa1
Farren <fcasad@gmail.com>
2018-08-29 23:00:14 -0700

    browserify (commonjs)

--- public/js/bootstrap.js
+++
@@ -1,9 +0,0 @@
-requirejs.config({
-  baseUrl: 'js',
-  paths: {
-    react: 'https://unpkg.com/react@15.6.2/dist/react',
-    'react-dom': 'https://unpkg.com/react-dom@15.6.2/dist/react-dom',
-  },
-});
-
-requirejs(['./main']);
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var TodoList = require('./components/TodoList');
+
+ReactDOM.render(React.createElement(TodoList), document.getElementById('root'));
